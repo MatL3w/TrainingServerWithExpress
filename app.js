6610412af89666fb337bf991869c12ef5b7b87fa
Lecislav <lecislav@protonmail.com>
2022-11-01 20:48:28 +0100

    next()

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 const express = require('express');
 
+const http = require('http');
+
 const app = express();
+
+app.use((req,res,next)=>{
+    console.log("im in middleware");
+    next();
+});
+
+app.use((req, res, next) => {
+  console.log("im in another middleware");
+});
+
+const server = http.createServer(app);
+
+server.listen(3000);
