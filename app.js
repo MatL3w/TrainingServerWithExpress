97cdc2d2a12ddbe7c319da288c2ee50eee09f883
Lecislav <lecislav@protonmail.com>
2022-11-14 21:20:25 +0100

    add 404 message

--- app.js
+++ app.js
@@ -10,7 +10,12 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(AdminRoutes);
-app.use(shopRoutes);
+//app.use(shopRoutes);
 
+app.use((req,res, next) =>{
+    res.sendStatus(404);
+    // res.send('<h2>Page not found</h2>');
+    // next();
+});
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -5,7 +5,7 @@ const router = express.Router();
 router.use("/", (req, res, next) => {
   console.log("im in middleware");
   res.send("<h1>Hello</h1>");
-  next();
+  //next();
 });
 
 
