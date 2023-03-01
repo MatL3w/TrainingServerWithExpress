d14bc9720e14824cca8daf769219ad4d000a8eb0
Lecislav <lecislav@protonmail.com>
2023-03-01 13:51:37 +0100

    *

--- routes/admin.js
+++ routes/admin.js
@@ -7,7 +7,7 @@ const router = express.Router();
 const products = [];
 
 router.get("/add-product", (req, res, next) => {
-  res.render('add-product',{pageTitle:'Add product'});
+  res.render('add-product',{docTitle:'Add product'});
 });
 
 router.post("/add-product", (req, res) => {
