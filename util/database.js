9bb87ce0fa8d8285de6fb22cffbb702e294c1ceb
Lecislav <lecislav@protonmail.com>
2023-03-09 18:02:57 +0100

    mongoDb

--- util/database.js
+++ util/database.js
@@ -1,8 +1,25 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient  = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+let _db;
 
-module.exports = sequelize;
+exports.mongoConnect =()=>{
+  MongoClient.connect("mongodb+srv://lecislav:usZSIcaVCbS1WPxK@cluster0.cehtsby.mongodb.net/?retryWrites=true&w=majority")
+    .then(client => {
+      console.log("connected");
+      _db=client.db();
+    })
+    .catch((err) => {
+      console.log(err)
+      throw err;
+    });
+}
+
+exports.getDb=()=>{
+  if(_db){
+    return _db;
+  }
+  throw "no database found";
+}
+
+
